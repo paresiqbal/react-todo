f169ce1afcafc4326bad6c371fa487bb7d50097c
paresiqbal <thecrowdel@gmail.com>
2023-04-11 04:53:22 +0700

    adding Custom form

---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,10 @@
+export default function CustomForm() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="taskName">Task Name : </label>
+        <input type="text" name="taskName" id="taskName" />
+      </div>
+    </form>
+  );
+}
